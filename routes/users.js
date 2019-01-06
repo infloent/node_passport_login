c6d3bccd14c5795540d8a85df58d953bb5c759ee
Florin Raducan <florin.raducan@netcentric.biz>
2019-01-06 17:48:11 +0200

    Add authentification & login/logout

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // User module
 const User = require('../model/User');
@@ -78,6 +79,22 @@ router.post('/register', (req, res) => {
             }
         });
    }
+});
+
+// Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'Yout are logged out');
+    res.redirect('/users/login');
 });
 
 module.exports = router;
